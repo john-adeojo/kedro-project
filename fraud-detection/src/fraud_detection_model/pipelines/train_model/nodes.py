05d4d933170bae51379494bb46385195dd41f630
john-adeojo <johnadeojo91@gmail.com>
2023-06-28 02:34:51 +0100

    Fixed bugs

--- fraud-detection/src/fraud_detection_model/pipelines/train_model/nodes.py
+++ fraud-detection/src/fraud_detection_model/pipelines/train_model/nodes.py
@@ -161,21 +161,7 @@ def model_training_diagnostics(full_predictions: pd.DataFrame, output_dir) -> Tu
     )
 
     roc_curve_plot = fig
-    
-#     fpr, tpr, thresholds = roc_curve(full_predictions['Class'], full_predictions['Class_predictions'])
-#     roc_auc = auc(fpr, tpr)
-
-#     plt.figure()
-#     plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-#     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-#     plt.xlim([0.0, 1.0])
-#     plt.ylim([0.0, 1.05])
-#     plt.xlabel('False Positive Rate')
-#     plt.ylabel('True Positive Rate')
-#     plt.title('Receiver Operating Characteristic')
-#     plt.legend(loc="lower right")
-#     roc_curve_plot = plt.gcf()
-    
+        
     # plot loss curve
     latest_experiment_dir = get_latest_experiment_dir(output_dir)
 
